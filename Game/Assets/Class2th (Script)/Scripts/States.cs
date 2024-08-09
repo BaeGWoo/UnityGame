9658c6900566851d27a2cf3ab04ab519316a5062
BaeGWoo <gwbae4211@naver.com>
2024-08-09 15:45:25 +0900

    (2024.08.09) 스크립트 파일 생성

---
+++ Game/Assets/Class2th (Script)/Scripts/States.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data
+{
+    private int health;
+    protected string name;
+
+    public Data(int health, string name)
+    {
+        this.health = health;
+        this.name = name;
+    }
+
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}
+
+public class States : MonoBehaviour
+{
+    Data data=new Data(100,"Dinasaur");
+    public int attack=30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Attack 변수의 값은 " + attack);
+        Debug.Log("Data의 이름 "+data.Name);
+
+    }
+
+   
+    
+}
