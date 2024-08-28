229f7aac1fec23996ba102fcb90012cb53257bf2
BaeGWoo <gwbae4211@naver.com>
2024-08-28 15:18:42 +0900

    (2024.08.28) 라이트

---
+++ Game/Assets/Class13th (Light)/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+
+
+    void Start()
+    {
+        prefab = Instantiate(Resources.Load<GameObject>("Pier"));
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
